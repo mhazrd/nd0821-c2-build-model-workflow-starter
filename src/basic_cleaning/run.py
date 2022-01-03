18415c114b8ecf59cd7b815e3f07a34cad01df94
sang <murdenh1999@gmail.com>
2022-01-03 11:57:35 +1100

    Fix bug

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -41,6 +41,8 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
 
     logger.info(f"Upload the cleaned dataset as {args.output_artifact} in W&B")
